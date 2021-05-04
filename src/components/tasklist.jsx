a579a0398bb67a8c76cdd141cb7aeb52f523a939
Lucabrandimarti.00@gmail.com <Lucabrandimarti.00@gmail.com>
2021-05-04 17:47:49 +0200

    [DELETE] pointless components

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,45 +1,41 @@
-import React from "react";
 import Task from "./task";
 
-const TaskList = (props) => {
-  const taskList = props.taskArray.map((task, index) => (
+const TaskList = ({ taskArray, removeTask, taskStatusSwitch }) => {
+  const tList = taskArray.map((task, index) => (
     <li>
-      {" "}
       <Task
         index={index}
         key={index}
         id={task.id}
         text={task.text}
-        deadline = {task.deadline}
-        remove={props.remove}
-        completeTask={props.completeTask}
+        deadline={task.deadline}
+        removeTask={removeTask}
+        taskStatusSwitch={taskStatusSwitch}
         completed={task.completed}
       />
     </li>
   ));
 
-  
-  const filteredArray = props.taskArray.filter(
-    (elements) =>  elements.text.indexOf(props.searchTask )!== -1 );
-  const filteredTaskList = filteredArray.map((task, index) => (
+  const filteredArray = taskArray.filter(
+    (elements) => elements.text.indexOf(props.searchTask) !== -1
+  );
+
+  const filteredTlist = filteredArray.map((task, index) => (
     <li>
-      {" "}
       <Task
         index={index}
         key={index}
         id={task.id}
         text={task.text}
-        time = {task.time}
-        remove={props.remove}
-        completeTask={props.completeTask}
+        deadline={task.deadline}
+        removeTask={removeTask}
+        taskStatusSwitch={taskStatusSwitch}
         completed={task.completed}
       />
     </li>
   ));
 
-  return (
-  <ul>{(filteredArray.length >=1)? filteredTaskList:taskList}</ul>
-  )
+  return <ul>{filteredArray.length >= 1 ? filteredTlist : tList}</ul>;
 };
 
 export default TaskList;
