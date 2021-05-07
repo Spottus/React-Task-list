c3ce1a4213979be5723e24cc538e40807061ef44
Lucabrandimarti.00@gmail.com <Lucabrandimarti.00@gmail.com>
2021-05-07 12:36:19 +0200

    [UPDATE] list rendering logic

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,42 +1,30 @@
 import Task from "./task";
 
-const TaskList = ({ taskArray, removeTask, taskStatusSwitch,searchTask}) => {
-  const tList = taskArray.map((task, index) => (
-    <li>
-      <Task
-        index={index}
-        key={index}
-        id={task.id}
-        text={task.text}
-        deadline={task.deadline}
-        completed={task.completed}
-        removeTask={removeTask}
-        taskStatusSwitch={taskStatusSwitch}
-      />
-    </li>
-  ));
-
-  const filteredArray = taskArray.filter(
+const TaskList = ({ taskArray, removeTask, taskStatusSwitch, searchTask }) => {
+  
+  const targets = taskArray.filter(
     (elements) => elements.text.indexOf(searchTask) !== -1
   );
 
-  const filteredTlist = filteredArray.map((task, index) => (
-    <li>
-      <Task
-        index={index}
-        key={index}
-        id={task.id}
-        text={task.text}
-        deadline={task.deadline}
-        completed={task.completed}
-        removeTask={removeTask}
-        taskStatusSwitch={taskStatusSwitch}
-      
-      />
-    </li>
-  ));
-
-  return <ul>{filteredArray.length >= 1 ? filteredTlist : tList}</ul>;
+  const makeList = (arr) =>
+  {
+    return arr.map((task, index) => (
+      <li>
+        <Task
+          index={index}
+          key={index}
+          id={task.id}
+          text={task.text}
+          deadline={task.deadline}
+          completed={task.completed}
+          removeTask={removeTask}
+          taskStatusSwitch={taskStatusSwitch}
+        />
+      </li>
+    ));
+  }
+ 
+  return <ul>{targets!==null? makeList(targets):makeList(taskArray)}</ul>;
 };
 
 export default TaskList;
